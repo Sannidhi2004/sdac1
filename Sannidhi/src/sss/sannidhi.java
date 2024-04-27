b5b73722d7dd04fe610bb286989ffc7fc2c419ef
sdac-14 <sdac-14@sdac-14-PC>
2024-04-27 14:54:28 +0530

    commit

---
+++ Sannidhi/src/sss/sannidhi.java
@@ -0,0 +1,8 @@
+package sss;
+
+public class sannidhi {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
---
+++ sannidhi1/src/jik/ssss.java
@@ -0,0 +1,8 @@
+package jik;
+
+public class ssss {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
